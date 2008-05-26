e87e97cbb1b226a32267158b4883aecee4de6f08
Matt Foster <matt.p.foster@gmail.com>
2008-05-26 22:07:51 +0100

    • Rewrote help command in ruby • Deals with ambiguous matches, and now uses javascript to link to alternatives • Also added search box for ambiguous matches • Added support command to get gnuplot help output, and convert it to html (using bluecloth) if called directly. • Finally, rearranged the menus a bit.

---
+++ Support/bin/get_gnuplot_help.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby -w
+
+require 'rubygems'
+require 'bluecloth'
+
+def convert(word = '')
+  help =  "## Help for \'#{word}\'\n"
+  help << `export PAGER=cat; echo help #{word} | gnuplot 2>&1`
+end
+
+if __FILE__ == $0
+  word = ARGV.shift || STDIN.read
+  
+  help = convert(word)
+  bc = BlueCloth.new(help)
+  puts bc.to_html
+end
