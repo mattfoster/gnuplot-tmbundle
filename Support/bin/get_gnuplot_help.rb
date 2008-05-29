4eb5c201afa86cee41f0dab150eec8f78c5120a6
Matt Foster <matt.p.foster@gmail.com>
2008-05-29 15:36:46 +0100

    • Added a search bar to the top of every help page • Rearranged the page order a bit • Added require 'TextMate' to try and cope with the changes to the textmate libraries.

--- Support/bin/get_gnuplot_help.rb
+++ Support/bin/get_gnuplot_help.rb
@@ -1,17 +1,24 @@
 #!/usr/bin/env ruby -w
 
 require 'rubygems'
-require 'escape'
+# require 'escape'
 require 'bluecloth'
 
 def convert(word = '')
   help =  "## Help for \'#{word}\'\n"
-  help << htmlize(`export PAGER=cat; echo help #{word} | gnuplot 2>&1`)
+  help << `export PAGER=cat; echo help #{word} | gnuplot 2>&1`
 end
 
 if __FILE__ == $0
   word = ARGV.shift || STDIN.read
   help = convert(word)
+  
+  help.gsub!(/^\s*Syntax:\s*$/, "\n### Syntax\n")
+  help.gsub!(/^\s*Examples:\s*$/, "\n### Examples\n")
+  help.gsub!(/^\s*Example:\s*$/, "\n#### Example\n")
+  help.gsub!(/^\s*Subtopics available for .*:\s*$/, "\n#### Subtopics\n")
+  help.gsub!(/^\s*See also/, "\n#### See Also\n\t")
+  
   bc = BlueCloth.new(help)
   puts bc.to_html
 end
