cbf52cc618d0c2bf51af7ffeb830822a0714f97c
Matt Foster <matt.p.foster@gmail.com>
2008-11-10 11:08:37 +0000

    changed files to use new markdown convertor. You may need to update your support directory.

--- Support/bin/get_gnuplot_help.rb
+++ Support/bin/get_gnuplot_help.rb
@@ -1,17 +1,20 @@
 #!/usr/bin/env ruby -w
 
 $LOAD_PATH << "#{ENV['TM_SUPPORT_PATH']}/lib"
+$LOAD_PATH << "#{ENV['TM_SUPPORT_PATH']}/lib/tm"
 $LOAD_PATH << "#{ENV['TM_BUNDLE_SUPPORT']}/lib"
 
-require 'rubygems'
-require 'bluecloth'
+# require 'rubygems'
+# require 'bluecloth'
+require 'markdown'
 require 'gnuplot_help'
 include Gnuplot
+include TextMate
 
 if __FILE__ == $0
   word = ARGV.shift || STDIN.read
   help = convert(word)
-  
-  bc = BlueCloth.new(help)
-  puts bc.to_html
+  puts Markdown.to_html(help)
+  # bc = BlueCloth.new(help)
+  # puts bc.to_html
 end
